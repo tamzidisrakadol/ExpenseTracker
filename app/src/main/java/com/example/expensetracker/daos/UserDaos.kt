4db727a64faa385ae0ed24bc2ac208211e6f27c9
Tamzid Israk <tamzidisrak12@gmail.com>
2023-02-22 20:59:27 +0600

    Expense Tracker add google sign in to enter app

---
+++ app/src/main/java/com/example/expensetracker/daos/UserDaos.kt
@@ -0,0 +1,22 @@
+package com.example.expensetracker.daos
+
+import com.example.expensetracker.model.User
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class UserDaos {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val userCollection = db.collection("users")
+
+
+    //save data to FireStore
+    fun addUser(user: User?) {
+        user?.let {
+            GlobalScope.launch {
+                userCollection.document(user.uId).set(it)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/expensetracker/views/LoginActivity.kt
+++ app/src/main/java/com/example/expensetracker/views/LoginActivity.kt
@@ -1,18 +1,101 @@
 package com.example.expensetracker.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.expensetracker.R
+import com.example.expensetracker.daos.UserDaos
 import com.example.expensetracker.databinding.ActivityLoginBinding
+import com.example.expensetracker.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityLoginBinding
+    private val RC_SIGN_IN: Int =123
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient:GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.your_web_client_id))
+            .requestEmail()
+            .build()
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+        binding.signInBtn.setOnClickListener {
+            signInWithGoogle()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser!=null){
+            updateUI()
+        }
+    }
+
+
+    private fun signInWithGoogle() = lifecycleScope.launch(Dispatchers.IO) {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private  fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        val authResult = firebaseAuth.signInWithCredential(credential)
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                lifecycleScope.launch(Dispatchers.IO) {
+                    firebaseAuthWithGoogle(account!!)
+                    withContext(Dispatchers.Main){
+                       updateUI()
+                    }
+                }
+            } catch (e: ApiException) {
+                Log.w("TAG", "Google sign in failed", e)
+            }
+        }
+    }
+
+
+    private fun updateUI(){
+        val fUser = FirebaseAuth.getInstance().currentUser
+        if (fUser!=null){
+            val user = User(fUser.uid,fUser.displayName,fUser.photoUrl.toString())
+            val userDaos = UserDaos()
+            userDaos.addUser(user)
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
