c5acac1bd38e74041803468298770fc015355b7c
Tamzid Israk <tamzidisrak12@gmail.com>
2023-03-11 23:03:23 +0600

    Expense Tracker add google sign in to enter app add user to firestore and create Transition.kt (model class) add ExpenseDao and create AddExpenseActivity.kt to add all transition to firestore all Transition.kt will show in MainActivity.kt as a list and user can also see remaining balance. add delete option to delete single Transition.kt add piechart add reset btn to delete all transition update design of splash screen MainActivity.kt and AddExpenseActivity.kt add userImg to circleImgview
    ****Testing*****
    upload data to firestore
    showing list of data, date has been serializable
    date has been converted and showing in RecyclerView
    tracking data classes by month
    spinner with list of month
    fetching data by monthly
    setting a text while there is an element in the list or not
    create a new ReminderActivity.kt
    create layout for Reminder list
    create Entity,Dao,Database for ReminderDatabase.kt
    successfully inserting reminder data to room database
    showing simple notification after saving data to ROOM

--- app/src/main/java/com/example/expensetracker/views/ReminderActivity.kt
+++ app/src/main/java/com/example/expensetracker/views/ReminderActivity.kt
@@ -1,14 +1,20 @@
 package com.example.expensetracker.views
 
-import android.app.DatePickerDialog
-import android.app.ProgressDialog
-import android.app.TimePickerDialog
+import android.Manifest
+import android.app.*
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TimePicker
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.room.RoomDatabase
 import com.example.expensetracker.R
@@ -27,6 +33,12 @@ class ReminderActivity : AppCompatActivity() {
     private var timeChange: Boolean = false
     private lateinit var castToDate: Date
     private lateinit var castToTime:Date
+    private val CHANNEL_ID = "ChannelId"
+    private val CHANNEL_NAME = "channelname"
+    private val NOTIFICATION_ID = 0
+    private lateinit var notification:Notification
+    private lateinit var  notificationManager: NotificationManagerCompat
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +47,24 @@ class ReminderActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         db=ReminderDatabase.getDatabase(this)
+
+        createNotification()
+        notification = NotificationCompat.Builder(this@ReminderActivity,CHANNEL_ID)
+            .setContentTitle("Successful")
+            .setContentText("Reminder has been saved")
+            .setSmallIcon(R.drawable.data_save)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+         notificationManager = NotificationManagerCompat.from(this@ReminderActivity)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
 
         binding.selectDateTV.setOnClickListener {
             pickUpDate(it)
@@ -47,6 +77,8 @@ class ReminderActivity : AppCompatActivity() {
         binding.saveBtn.setOnClickListener {
             saveDataToRoom()
         }
+
+
 
 
     }
@@ -105,9 +137,21 @@ class ReminderActivity : AppCompatActivity() {
                 mProgressDialog.setTitle("keep patience")
                 mProgressDialog.setMessage("Saving....")
                 mProgressDialog.show()
-                delay(2000L)
-                Toast.makeText(this@ReminderActivity, "successfully inserted", Toast.LENGTH_SHORT).show()
-                mProgressDialog.hide()
+                delay(3000L)
+                mProgressDialog.dismiss()
+
+                //showing notification after saving data to ROOM
+                notificationManager = NotificationManagerCompat.from(this@ReminderActivity)
+                if (ActivityCompat.checkSelfPermission(
+                        this@ReminderActivity,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return@launch
+                }
+                notificationManager.notify(NOTIFICATION_ID,notification)
+
+
                 finish()
 
             }
@@ -115,6 +159,21 @@ class ReminderActivity : AppCompatActivity() {
             Toast.makeText(this@ReminderActivity, "please fill all the requirements", Toast.LENGTH_SHORT).show()
         }
     }
+
+
+    //creating notification channel
+    private fun createNotification(){
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+
+    }
+
 
 }
 
