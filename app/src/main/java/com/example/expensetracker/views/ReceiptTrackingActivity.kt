ee11c94296658f30730f1d4bab27a25cede7255d
Tamzid Israk <tamzidisrak12@gmail.com>
2023-08-14 22:00:05 +0600

    add layout of receipt tracking activity

--- app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
+++ app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
@@ -3,10 +3,15 @@ package com.example.expensetracker.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.expensetracker.R
+import com.example.expensetracker.databinding.ActivityRecieptTrackingBinding
 
 class ReceiptTrackingActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRecieptTrackingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reciept_tracking)
+        binding = ActivityRecieptTrackingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
