a9e3a94de86e9e6003ee0166f82dc4ef12dd921f
Tamzid Israk <tamzidisrak12@gmail.com>
2023-08-15 21:38:02 +0600

    Expense Tracker v.2
    Feature:
    1.Google SignIn
    2.Show Current Balance, pieChart, profile pic in MainActivity.kt
    3.Show List of Transaction by month-wise in ShowTransactionListActivity.kt
    4.Add transaction by setting values in AddExpenseActivity.kt
    5.Add Reset Btn to clear all transaction in current month
    6.Add ProfileActivity and sign out functionality
    7.checking NetworkConnection
    
    Fix:
    1.unable to Sign in
    2.not getting list by month-wise
    3.crashed for not setting value in AddExpenseActivity.kt
    4.TimeStamp for setting date
    5.conflict between selected month by current month
    6.showing month-wise current balance and pieChart in MainActivity
    7.capture or choose images for ReceiptTrackingActivity

--- app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
+++ app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
@@ -1,17 +1,92 @@
 package com.example.expensetracker.views
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.expensetracker.R
 import com.example.expensetracker.databinding.ActivityRecieptTrackingBinding
+import java.io.IOException
 
 class ReceiptTrackingActivity : AppCompatActivity() {
     private lateinit var binding:ActivityRecieptTrackingBinding
+    private val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecieptTrackingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.addReceiptbtn.setOnClickListener {
+
+        }
+        binding.imageView.setOnClickListener {
+            openCameraOrGallery()
+        }
 
     }
+
+
+    private fun openCameraOrGallery() {
+        val options = arrayOf("Take Photo", "Choose from Gallery", "Cancel")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Select Option")
+        builder.setItems(options) { dialog, item ->
+            when (options[item]) {
+                "Take Photo" -> openCamera()
+                "Choose from Gallery" -> openGallery()
+                "Cancel" -> dialog.dismiss()
+            }
+        }
+        builder.show()
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent,100)
+    }
+
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap?
+                    if (imageBitmap != null) {
+                        binding.imageView.setImageBitmap(imageBitmap)
+                    } else {
+                        Toast.makeText(this, "Failed to get image from camera", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                100 -> {
+                    if (data != null && data.data != null) {
+                        val imageUri = data.data
+                        try {
+                            val imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+                            binding.imageView.setImageBitmap(imageBitmap)
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(this, "Action canceled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
