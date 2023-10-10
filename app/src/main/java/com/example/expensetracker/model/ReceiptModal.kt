d2d8daa6d4ea08173181f4820da5012c907533a7
Tamzid Israk <tamzidisrak12@gmail.com>
2023-10-10 21:45:40 +0600

    fix simple bugs add date to receipt tracking

--- app/src/main/java/com/example/expensetracker/model/ReceiptModal.kt
+++ app/src/main/java/com/example/expensetracker/model/ReceiptModal.kt
@@ -3,5 +3,6 @@ package com.example.expensetracker.model
 data class ReceiptModal (
     val uid:String="",
     val receiptId:String="",
-    val imgUrl:String = ""
+    val imgUrl:String = "",
+    val date:String = ""
 )
--- app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
+++ app/src/main/java/com/example/expensetracker/views/ReceiptTrackingActivity.kt
@@ -7,13 +7,10 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.expensetracker.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.expensetracker.databinding.ActivityRecieptTrackingBinding
 import com.example.expensetracker.model.ReceiptModal
 import com.google.firebase.auth.FirebaseAuth
@@ -21,19 +18,26 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.util.Calendar
 import java.util.UUID
 
 class ReceiptTrackingActivity : AppCompatActivity() {
     private lateinit var binding:ActivityRecieptTrackingBinding
     private val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var imgUri:Uri
+    private var selectImage = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecieptTrackingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.addReceiptbtn.setOnClickListener {
-            uploadReceipt()
+            if (selectImage){
+                uploadReceipt()
+            }else{
+                Toast.makeText(this, "Select Receipt first", Toast.LENGTH_SHORT).show()
+            }
+
         }
         binding.imageView.setOnClickListener {
             openCameraOrGallery()
@@ -79,7 +83,9 @@ class ReceiptTrackingActivity : AppCompatActivity() {
                     if (imageBitmap != null) {
                         binding.imageView.setImageBitmap(imageBitmap)
                         imgUri = getImageUri(this, imageBitmap)
+                        selectImage = true
                     } else {
+                        selectImage = false
                         Toast.makeText(this, "Failed to get image from camera", Toast.LENGTH_SHORT).show()
                     }
                 }
@@ -89,7 +95,9 @@ class ReceiptTrackingActivity : AppCompatActivity() {
                         try {
                             val imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imgUri)
                             binding.imageView.setImageBitmap(imageBitmap)
+                            selectImage = true
                         } catch (e: IOException) {
+                            selectImage = false
                             e.printStackTrace()
                         }
                     }
@@ -110,7 +118,6 @@ class ReceiptTrackingActivity : AppCompatActivity() {
 
 
     private fun uploadReceipt(){
-
         val progressDialog= ProgressDialog(this);
         progressDialog.setTitle("Just a moment")
         progressDialog.setMessage("saving the receipt")
@@ -121,7 +128,7 @@ class ReceiptTrackingActivity : AppCompatActivity() {
         val storageRef = FirebaseStorage.getInstance().getReference("images/$receiptId")
         storageRef.putFile(imgUri).addOnSuccessListener {
             storageRef.downloadUrl.addOnSuccessListener {
-                Log.d("url", it.toString())
+
                 saveToFireStore(currentUserID!!,receiptId,it.toString())
             }
             Toast.makeText(this, "upload successfully", Toast.LENGTH_SHORT).show()
@@ -135,15 +142,15 @@ class ReceiptTrackingActivity : AppCompatActivity() {
             progressDialog.dismiss()
         }
 
-
-
-
     }
 
-
-
     private fun saveToFireStore(uid:String,receiptId:String,imgUrl:String){
-        val receiptModal = ReceiptModal(uid = uid, receiptId = receiptId, imgUrl = imgUrl)
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1 // Months are zero-based
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val currentDate ="$day/$month/$year"
+        val receiptModal = ReceiptModal(uid = uid, receiptId = receiptId, imgUrl = imgUrl, date = currentDate)
         FirebaseFirestore.getInstance().collection("Receipt").add(receiptModal)
 
     }
